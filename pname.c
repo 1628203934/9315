71847f27df5ea0d6cc419d5585a82ae545046ba8
Adam <116777037+1628203934@users.noreply.github.com>
2024-03-09 00:40:01 +1100

    Add files via upload

--- pname.c
+++ pname.c
@@ -2,39 +2,35 @@
 #include "fmgr.h"
 #include "utils/builtins.h"
 #include "lib/stringinfo.h"
+#include "access/hash.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 #include <ctype.h>
+#include <regex.h>
 
 PG_MODULE_MAGIC;
 
-bool is_valid_name(char* name);
-static int name_is_equal(char* a, char* b);
 // PersonName definition
 typedef struct PersonName
 {
-    text *family;
-    text *given;
+    int name_length;
+    char name[1];
 } PersonName;
 
-bool is_valid_name(char* name) {
-    char* temp;
-    temp = pstrdup(name);
-    size_t len;
-    len = strlen(name);
-    if (name[len - 1] == ' ' || len <= 1 || !isupper(name[0])) return false;
-    for (int i = 1; i < len; i++) {
-        if (!(isalpha(name[i]) || name[i] == '-' || name[i] == '\'' || name[i] == ' ') || (name[i] == ' ' && name[i + 1] == ' ')) return false;
-    }
-    temp = strtok(temp, " ");
-    while (temp != NULL) {
-        if (strlen(temp) < 2) {
-            return false;
+bool validName(char *str){
+    char *regex_pattern = "^[A-Z][A-Za-z'-]+([ ][A-Z][A-Za-z'-]+)*,[ ]?[A-Z][A-Za-z'-]+([ ][A-Z][A-Za-z'-]+)*$";
+    regex_t regex;
+    int flag;
+    bool result = false;
+    flag = regcomp(&regex, regex_pattern, REG_EXTENDED | REG_NOSUB);
+    if (flag == 0) {
+        if (!regexec(&regex, str, 0, NULL, 0)){
+            result = true;
         }
-        temp = strtok(NULL, " ");
     }
-    return true;
+    regfree(&regex);
+    return result;
 }
 
 // Function definition
@@ -44,31 +40,24 @@ Datum
 pname_in(PG_FUNCTION_ARGS)
 {
     char *str = PG_GETARG_CSTRING(0);
-    char *str_dup = pstrdup(str);
-    char *comma = strchr(str, ',');
-    // check validity of person name: 1. no comma 2. multiple commas 3. no family name 4. no given name 5. too many spaces after comma
-    if (!comma || strchr(comma + 1, ',') || comma == str || *(comma + 1) == '\0' || *(comma + 2) == ' ')
+    // to check whether input is valid
+    if (!validName(str))
         ereport(ERROR,
                 (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                        errmsg("invalid input syntax for type PersonName: \"%s\"", strdup)));
-    // separate person name into family and given names
-    char *family = strtok(str_dup, ",");
-    char *given = strtok(NULL, ",");
-    if (*(comma + 1) == ' ') given = comma + 2;
-    if (given == NULL)
-        ereport(ERROR,
-                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                        errmsg("Incorrect name")));
-    // check validity of family and given names respectively
-    if (!(is_valid_name(family) && is_valid_name(given)))
-        ereport(ERROR,
-                (errcode(ERRCODE_INVALID_TEXT_REPRESENTATION),
-                        errmsg("Incorrect name")));
-    // store two names using self-defined structure Pname
-    PersonName *input = (PersonName *) palloc(sizeof(PersonName));
-    input->family = cstring_to_text(family);
-    input->given = cstring_to_text(given);
-    PG_RETURN_POINTER(input);
+                        errmsg("invalid input syntax for type %s: \"%s\"",
+                               "PersonName", str)));
+    char *given = strchr(str, ',');
+    int family_len = given - str;
+    if (*(given + 1) == ' ') given += 2;
+    else given++;
+    int length = family_len + strlen(given) + 2;
+    PersonName *result = (PersonName *) palloc(VARHDRSZ + length);
+    SET_VARSIZE(result, VARHDRSZ + length);
+    str[family_len] = '\0';
+    snprintf(result->name, length , "%s,%s", str, given);
+    str[family_len] = ',';
+    result->name_length = length - 1;
+    PG_RETURN_POINTER(result);
 }
 
 PG_FUNCTION_INFO_V1(pname_out);
@@ -77,45 +66,21 @@ Datum
 pname_out(PG_FUNCTION_ARGS)
 {
     PersonName *pname = (PersonName *) PG_GETARG_POINTER(0);
-    char *family = text_to_cstring(pname->family);
-    char *given = text_to_cstring(pname->given);
-    char *output = psprintf("%s, %s", family, given);
-    PG_RETURN_CSTRING(output);
-}
-
-static int name_is_equal (char *a, char *b) {
-    for (int i = 0; i < (strlen(a) > strlen(b) ? strlen(b) : strlen(a)); i++) {
-        if (a[i] > b[i]) {
-            return 1;
-        } else if (a[i] < b[i]){
-            return -1;
-        }
-    }
-    if (strlen(a) == strlen(b)) {
-        return 0;
-    } else if (strlen(a) > strlen(b)) {
-        return 1;
-    } else {
-        return -1;
-    }
+    PG_RETURN_CSTRING(pname->name);
 }
 
 static int
 pname_cmp_internal(PersonName *a, PersonName *b)
 {
-    char *f_a = text_to_cstring(a->family);
-    char *g_a = text_to_cstring(a->given);
-    char *f_b = text_to_cstring(b->family);
-    char *g_b = text_to_cstring(b->given);
-
-    if (name_is_equal(f_a, f_b) == 1) return 1;
-    else if (name_is_equal(f_a, f_b) == -1) return -1;
-    else {
-        if (name_is_equal(g_a, g_b) == 1) return 1;
-        else if (name_is_equal(g_a, g_b) == -1) return -1;
-        else return 0;
-    }
-    return 0;
+    char *comma_a = strchr(a->name, ',');
+    char *comma_b = strchr(b->name, ',');
+    int len_a = comma_a - a->name; // family name length
+    int len_b = comma_b - b->name;
+    int family_cmp = strncmp(a->name, b->name, len_a < len_b ? len_a : len_b); // compare family names
+    if (family_cmp == 0) {
+        if (len_a == len_b) return strcmp(comma_a + 1, comma_b + 1); // family names are equal, compare given names;
+        else return len_a > len_b ? 1 : -1;
+    } else return family_cmp < 0 ? -1 : 1;
 }
 
 PG_FUNCTION_INFO_V1(pname_lt);
@@ -184,13 +149,29 @@ pname_gt(PG_FUNCTION_ARGS)
     PG_RETURN_BOOL(pname_cmp_internal(a, b) > 0);
 }
 
+PG_FUNCTION_INFO_V1(pname_cmp);
+
+Datum
+pname_cmp(PG_FUNCTION_ARGS)
+{
+    PersonName    *a = (PersonName *) PG_GETARG_POINTER(0);
+    PersonName    *b = (PersonName *) PG_GETARG_POINTER(1);
+
+    PG_RETURN_INT32(pname_cmp_internal(a, b));
+}
+
 PG_FUNCTION_INFO_V1(family);
 
 Datum
 family(PG_FUNCTION_ARGS)
 {
     PersonName *pname = (PersonName *) PG_GETARG_POINTER(0);
-    PG_RETURN_TEXT_P(pname->family);
+    char *comma = strchr(pname->name, ',');
+    int family_length = comma - pname->name;
+    text *family = (text *) palloc(VARHDRSZ + family_length);
+    SET_VARSIZE(family, VARHDRSZ + family_length);
+    memcpy(VARDATA(family), pname->name, family_length);
+    PG_RETURN_TEXT_P(family);
 }
 
 PG_FUNCTION_INFO_V1(given);
@@ -199,7 +180,9 @@ Datum
 given(PG_FUNCTION_ARGS)
 {
     PersonName *pname = (PersonName *) PG_GETARG_POINTER(0);
-    PG_RETURN_TEXT_P(pname->given);
+    char *comma = strchr(pname->name, ',');
+    text *given = cstring_to_text(comma + 1);
+    PG_RETURN_TEXT_P(given);
 }
 
 PG_FUNCTION_INFO_V1(show);
@@ -208,13 +191,23 @@ Datum
 show(PG_FUNCTION_ARGS)
 {
     PersonName *pname = (PersonName *) PG_GETARG_POINTER(0);
-    char* family = text_to_cstring(pname->family);
-    char* given = text_to_cstring(pname->given);
-    char* first_given = strtok(given, " ");
-    int result_len = strlen(first_given) + strlen(family) + 2;
-    char *fullname = palloc(result_len);
-    text *result;
-    snprintf(fullname, result_len, "%s %s", first_given, family);
-    result = cstring_to_text(fullname);
+    char *full_name = pstrdup(pname->name);
+    char *family = strtok(full_name, ",");
+    char *given = strtok(NULL, ",");
+    char *first_given = strtok(given, " "); // extract the first part of given name splitted by space
+    int str_len = strlen(family) + strlen(first_given) + 2;
+    char *formatted_name = palloc(str_len);
+    snprintf(formatted_name, str_len, "%s %s", first_given, family);
+    text *result = cstring_to_text(formatted_name);
     PG_RETURN_TEXT_P(result);
+}
+
+PG_FUNCTION_INFO_V1(pname_hash);
+
+Datum
+pname_hash(PG_FUNCTION_ARGS)
+{
+    PersonName *pname = (PersonName *) PG_GETARG_POINTER(0);
+    int hash_code = DatumGetUInt32(hash_any((unsigned char *) pname->name, pname->name_length));
+    PG_RETURN_INT32(hash_code);
 }
